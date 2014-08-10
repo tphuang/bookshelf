787673d7561ce83c26c50c8ce6f5c88143bb5f46
Tingpeng <robin_1966@163.com>
2014-08-11 06:57:28 +0800

    Implement Douban Api with Java XMLparser

--- src/main/java/com/thoughtworks/bookshelf/service/App.java
+++ src/main/java/com/thoughtworks/bookshelf/service/App.java
@@ -1,24 +1,27 @@
 package com.thoughtworks.bookshelf.service;
 
-import com.thoughtworks.bookshelf.service.impl.FileServiceImpl;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.List;
-
 
 public class App {
     public static void main(String[] args) throws IOException {
         System.out.println("Hello World!");
+        new App("http://api.douban.com/book/subject/isbn/9787308083256");
 
-        FileService fileService = new FileServiceImpl();
-        List<File> imageFiles = fileService.readFile("/Users/tphuang/Projects/Java/bookshelf/src/main/webapp/images");
-        for (File file : imageFiles) {
-            System.out.println(file.getName() + "\t" + file.getPath());
-        }
-        System.out.println(imageFiles);
+    }
 
-        System.out.println("It's ok now!");
+    public App(String url){
+//        DefaultHttpClient client = new DefaultHttpClient();
+//        HttpGet get = new HttpGet(url);
+//        HttpResponse response = client.execute(get);
+//        HttpEntity entity = response.getEntity();
+//        InputStream is = entity.getContent();
+//        DoubanBookInfo book = new BookXMLParser(is).getBook();
+//        System.out.println("title:->" + book.getTitle());
+//        System.out.println("summary:->"+ book.getSummary());
+//        System.out.println("price:-->" + book.getPrice());
+//        System.out.println("author:-->" + book.getAuthor());
+//        System.out.println("ImagePath:-->" + book.getImagePath());
     }
 
 }
